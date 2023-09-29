0e85cc4b70ff4a6f1a55e209490ccf34c915023d
tanaka ken <tanaka-ken@netlab.jp>
2023-09-29 10:28:22 +0900

    設定画面のフォームの改行等のリファクタリング

--- lib/github_repositories_helper_patch.rb
+++ lib/github_repositories_helper_patch.rb
@@ -8,19 +8,30 @@ module GithubRepositoriesHelperPatch
 
   module InstanceMethods
     def github_field_tags(form, repository)
-      content_tag('p', form.text_field(:url, size: 60, required: true,
-                                       disabled: !repository.safe_attribute?('url')) +
-                       scm_path_info_tag(repository)) +
-        content_tag('p', form.password_field(
-                      :password, size: 60, name: 'ignore',
-                      label: l('redmine_github_adapter.label_api_token'), required: true,
-                      value: ((repository.new_record? || repository.password.blank?) ? '' : ('x' * 15)),
-                      onfocus: "this.value=''; this.name='repository[password]';",
-                      onchange: "this.name='repository[password]';")) +
-        content_tag('p', form.check_box(
-                      :report_last_commit,
-                      label: l(:label_git_report_last_commit)
-                    ))
+      field_tags = []
+      field_tags << content_tag(
+        'p',
+        form.text_field(:url, size: 60, required: true, disabled: !repository.safe_attribute?('url')) +
+        scm_path_info_tag(repository)
+      )
+      field_tags << content_tag(
+        'p',
+        form.password_field(
+          :password, size: 60, name: 'ignore',
+          label: l('redmine_github_adapter.label_api_token'), required: true,
+          value: ((repository.new_record? || repository.password.blank?) ? '' : ('x' * 15)),
+          onfocus: "this.value=''; this.name='repository[password]';",
+          onchange: "this.name='repository[password]';"
+        )
+      )
+      field_tags << content_tag(
+        'p',
+        form.check_box(
+          :report_last_commit,
+          label: l(:label_git_report_last_commit)
+        )
+      )
+      field_tags.join.html_safe
     end
   end
 end
