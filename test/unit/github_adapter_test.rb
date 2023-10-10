efec5484f514df6f329af4cd63a0a2bef12a8c4e
HiroyasuTawara <tawara@netlab.jp>
2023-10-10 02:12:55 +0000

    entriesメソッドのテストを修正

--- test/unit/github_adapter_test.rb
+++ test/unit/github_adapter_test.rb
@@ -77,6 +77,7 @@ class GithubAdapterTest < ActiveSupport::TestCase
 
   def test_entries_Githubの戻り値が空の場合
     Octokit.stub(:contents, build_mock([]) { |repos, path, ref|
+      assert_equal @repo, repos
     }) do
       entries = @scm.entries
       assert_equal 0, entries.length
@@ -84,7 +85,7 @@ class GithubAdapterTest < ActiveSupport::TestCase
   end
 
   def test_entries_Githubの戻り値が1つある場合
-    content = OctokitContent.new('test.md', 'dir/test.md', 'file', 256)
+    content = OctokitContent.new('test.md', 'farend/redmine_github_repo', 'file', 256)
     
     Octokit.stub(:contents, build_mock([content], []) { |repos, path, ref|
       assert_equal @repo, repos
@@ -92,7 +93,7 @@ class GithubAdapterTest < ActiveSupport::TestCase
       entries = @scm.entries
       assert_equal 1, entries.length
       assert_equal 'test.md', entries[0].name
-      assert_equal 'dir/test.md', entries[0].path
+      assert_equal 'farend/redmine_github_repo', entries[0].path
       assert_equal 'file', entries[0].kind
       assert_equal 256, entries[0].size
     end
@@ -100,7 +101,7 @@ class GithubAdapterTest < ActiveSupport::TestCase
 
   def test_entries_Githubの戻り値が複数ある場合
     contents = ['test.md', 'test.txt'].sort.map{ |name|
-      OctokitContent.new(name, "dir/#{name}", 'file', 256)
+      OctokitContent.new(name, 'farend/redmine_github_repo', 'file', 256)
     }
     
     Octokit.stub(:contents, build_mock(contents, []) { |repos, path, ref|
@@ -109,15 +110,15 @@ class GithubAdapterTest < ActiveSupport::TestCase
       entries = @scm.entries
       assert_equal 2, entries.length
       assert_equal 'test.md', entries[0].name
-      assert_equal 'dir/test.md', entries[0].path
+      assert_equal 'farend/redmine_github_repo', entries[0].path
       assert_equal 'test.txt', entries[1].name
-      assert_equal 'dir/test.txt', entries[1].path
+      assert_equal 'farend/redmine_github_repo', entries[1].path
     end
   end
 
   def test_entries_Githubに未ソートのコンテンツが与えられた場合
     contents = ['bbb.md', 'ccc.md', 'aaa.md'].map{ |name|
-      OctokitContent.new(name, "dir/#{name}", 'file', 256)
+      OctokitContent.new(name, 'farend/redmine_github_repo', 'file', 256)
     }
     
     Octokit.stub(:contents, build_mock(contents, []) { |repos, path, ref|
@@ -144,14 +145,19 @@ class GithubAdapterTest < ActiveSupport::TestCase
 
   def test_entries_Githubでオプションreport_last_commitがtrueの場合
     # TODO: commitのモックをテスト内で使用可能にする
-    # content = OctokitContent.new('test.md', 'dir/test.md', 'file', 256)
-    # commits = [OctokitCommit.new(sha: 'shashasha')]
-    # 
-    # Octokit.stub(:contents, build_mock([content], []) { |repos, path, ref|
-    #   assert_equal @repo, repos
-    # }) do
-    #   entries = @scm.entries(nil, 'shashasha', {report_last_commit: true})
-    # end
+    content = OctokitContent.new('test.md', 'farend/redmine_github_repo', 'file', 256)
+    lastrev = OctokitRevision.new(identifier: 'shashasha')
+    # = [OctokitCommit.new(sha: 'shashasha')]
+    
+    Octokit.stub(:contents, build_mock(content, []) { |repos, path, ref|
+      assert_equal @repo, repos
+    }) do
+      @scm.stub(:lastrev, build_mock(lastrev, []) { |repos, rev|
+        assert_equal @repo, repos
+      }) do
+        entries = @scm.entries(nil, 'shashasha', {report_last_commit: true})
+      end
+    end
   end
 
 
@@ -159,8 +165,8 @@ class GithubAdapterTest < ActiveSupport::TestCase
 
   OctokitBranch = Struct.new(:name, :commit, keyword_init: true)
   OctokitCommit = Struct.new(:sha, keyword_init: true)
-  OctokitContent = Struct.new(:name, :path, :type, :size, :lastrev)
-
+  OctokitContent = Struct.new(:name, :path, :type, :size)
+  OctokitRevision = Struct.new(:identifier, :paths)
 
   def build_mock(*returns, &proc)
     mock = Minitest::Mock.new
